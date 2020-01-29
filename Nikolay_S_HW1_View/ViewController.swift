83739b2273e3f17aa3e4f92fdaa2e3032af2e2e3
SorokinN <nikolay@sorokin.dev>
2020-01-29 18:22:07 +0300

    Initial Commit

---
+++ Nikolay_S_HW1_View/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nikolay_S_HW1_View
+//
+//  Created by Nikolay Sorokin on 29/01/2020.
+//  Copyright © 2020 Nikolay Sorokin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
